7cdc2f92465aa284d71773678c950c423d36338c
lschuermann <lnsm@users.noreply.github.com>
2015-10-10 16:30:24 +0200

    Add basic routes to the express-app.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+var config = require('../config.json');
 
-/* GET home page. */
+var express = require('express'),
+    hashids = require('hashids');
+
+var router = express.Router();
+    project_hashids = new hashids(config.hashids_salt);
+
+/* Overview-Page */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var projects = [{
+        title: "Pledgy",
+        desc: "Pure awesomeness."
+    }];
+    res.render('index', {
+        title: 'Pledgy',
+        projects: projects
+    });
+});
+
+/* Project-Page */
+router.get('/project/:project_uid', function(req, res, next) {
 });
 
 module.exports = router;
